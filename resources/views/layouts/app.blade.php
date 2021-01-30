70a5ba3bbc23398990d83dedf3b54c5b95e28acd
Maheeb <maheeb009@gmail.com>
2021-01-30 16:56:42 +0600

    Real time API setup done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
         </main>
     </div>
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" ></script>
     @stack('scripts')
 </body>
 
